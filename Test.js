4ad8af2cf00ba9e202cb625efa9efcedb751eba9
kkshin044 <044apde@gmail.com>
2022-01-10 19:55:18 +0900

    second commit

--- Test.js
+++ Test.js
@@ -2,3 +2,9 @@ for(let i = 0; i < 10; i++){
     console.log(i + 1);
   }  
   console.log(`Silence is language of god`);
+  console.log(`Silence is language of god`);
+  console.log(`Silence is language of god`);
+  console.log(`Silence is language of god`);
+  console.log(`Silence is language of god`);
+  console.log(`Silence is language of god`);
+
