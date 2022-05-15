c93cfe24309aa791f5b60580cf93f75be23b7a8c
kkshin044 <044apde@gmail.com>
2022-05-15 22:51:05 +0900

    update models, controllers for link DB

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,4 @@
-import User from "../../../Youtube-clone-coding/src/models/User";
-
-export const user = (req, res) => {
-  return res.render("user", { pageTitle: "USER PAGE" });
-};
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "JOIN" });
@@ -12,20 +8,41 @@ export const postJoin = async (req, res) => {
   const { name, email, password, password2 } = req.body;
   const pageTitle = "Join";
   if (password !== password2) {
-      return res.status(400).render("join", {
-          pageTitle,
-          errorMessage: "비밀번호가 서로 맞지 않습니다."
-      });
-  };
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "비빌번호가 서로 맞지 않습니다.",
+    });
+  }
   const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This email/username is already taken.",
+    });
+  }
+  try {
+    await User.create({
+      name,
+      email,
+      password,
+    });
+    return res.redirect("/login");
+  } catch {
+    return res.status(400).render("Join", {
+      pageTitle,
+      errorMessage: error._mesage,
+    });
+  }
+};
   
-  return res.render("join", { pageTitle: "JOIN" });
-};
+  export const login = (req, res) => {
+    return res.render("login", { pageTitle: "LOGIN" });
+  };
+  
+  export const user = (req, res) => {
+    return res.render("user", { pageTitle: "USER PAGE" });
+  };
 
-export const login = (req, res) => {
-  return res.render("login", { pageTitle: "LOGIN" });
-};
-
-export const editUser = (req, res) => {
-  return res.render("editUser", { pageTitle: "Edit user" });
-};
+  export const editUser = (req, res) => {
+    return res.render("editUser", { pageTitle: "Edit user" });
+  };
