5ded768ec0c3a09c8fc66bf4799a22c0160bef6b
kkshin044 <044apde@gmail.com>
2022-05-18 03:24:27 +0900

    update base.pug

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,7 @@
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
-  return res.render("test", { pageTitle: "JOIN" });
+  return res.render("base", { pageTitle: "JOIN" });
 };
 
 export const postJoin = async (req, res) => {
